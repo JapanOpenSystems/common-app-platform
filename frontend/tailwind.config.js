da70319e1f7d5053d3c32070cc05d7e983ae562a
imurata <imurata@jops.co.jp>
2025-07-08 15:11:10 +0900

    eidt 動的なCSSのクラスを明示的にした

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -12,4 +12,18 @@ export default {
   plugins: [
     require('@digital-go-jp/tailwind-theme-plugin')
   ],
+  safelist: [
+    'grid-cols-1',
+    'grid-cols-2', 
+    'grid-cols-3',
+    'grid-cols-4',
+    'grid-cols-5',
+    'grid-cols-6',
+    'grid-cols-7',
+    'grid-cols-8',
+    'grid-cols-9',
+    'grid-cols-10',
+    'grid-cols-11',
+    'grid-cols-12'
+  ]
 } 
